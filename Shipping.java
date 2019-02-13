fe59be4a205439b5a1529dab1432bf134b3f37d2
Yanni App13 <jonvincruman@gmail.com>
2019-02-13 15:27:28 -0800

    added scanner and Decimalformat classes

---
+++ Shipping.java
@@ -0,0 +1,21 @@
+/*
+Jonathan Manzano
+// Assignment: Selection Structures - Preliminary Part 1: Cost of Shipping
+/* This program is to calculate the price for a package depending on the 
+weight the user inputs.  The user is limited to input values greater than 0
+and less than or equal to 20.  Error messages re displayed when the user's 
+input is outside the accepted range*/
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Shipping {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        double weight, price;
+
+    }
+}
